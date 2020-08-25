562bb6eea57405a2bc261c05dd0ac907c6a62ee2
Natalia <natalia_vileyto@outlook.com>
2020-08-25 07:33:29 +0300

    слайдер + табы

--- src/js/lib/lib.js
+++ src/js/lib/lib.js
@@ -6,6 +6,10 @@ import './modules/actions';
 import './modules/effects';
 import './components/dropdown';
 import './components/modal';
+import './components/tab';
+import './components/accordion';
+import './components/carousel';
+import './services/request';
 
 
 export default $;
